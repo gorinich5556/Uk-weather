e542b474cf97cb5034b21eaba7232ad70e88d6d7
Egor Kovalev <egor.tharf@gmail.com>
2022-12-11 10:35:32 +0200

    add bottom sheet scaffold in other screens

--- app/src/main/java/com/example/ukweather/layout/todayScreen/todayScreen.kt
+++ app/src/main/java/com/example/ukweather/layout/todayScreen/todayScreen.kt
@@ -5,17 +5,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import androidx.navigation.NavController
 import com.example.ukweather.db.DbManager
 import com.example.ukweather.getWeather.climate
+import com.example.ukweather.layout.common.navigationBar
 import com.example.ukweather.layout.todayScreen.lazyRowForShowWeather
 import com.example.ukweather.ui.theme.backgroundDarkBlue
 import com.example.ukweather.ui.theme.backgroundLightBlue
@@ -23,31 +29,57 @@ import com.example.ukweather.ui.theme.backgroundLightBlue
 @Composable
 fun todayScreen(context: Context, climateState: MutableState<climate>, navController: NavController, nowClimate: MutableState<climate>) {
     val gradientGrayWhite = Brush.verticalGradient(0f to backgroundDarkBlue, 1000f to backgroundLightBlue)
+    val scaffoldState = rememberBottomSheetScaffoldState()
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(gradientGrayWhite)
     ) {
+        BottomSheetScaffold(
+            modifier = Modifier
+                .padding(bottom = 70.dp)
+                .zIndex(0.5f),
+            sheetContent = {
+                Box(
+                    modifier = Modifier
+                        .zIndex(1f),
+                    contentAlignment = Alignment.BottomCenter
+                ) {
+                    bottom(context, nowClimate)
+                }
+            },
+            sheetShape = RoundedCornerShape(topEnd = 30.dp, topStart = 30.dp),
+            scaffoldState = scaffoldState,
+            sheetPeekHeight = 50.dp,
+        ){
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(gradientGrayWhite)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ){
+                    lazyRowForShowWeather()
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .zIndex(2f),
+                    contentAlignment = Alignment.TopCenter
+                ) {
+                    changeLocate(nowClimate)
+                }
+            }
+        }
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .zIndex(2f),
             contentAlignment = Alignment.BottomCenter
-        ){
-            bottom(context = context, climateState = climateState)
-        }
-        Box(
-            modifier = Modifier
-                .fillMaxSize(),
-            contentAlignment = Alignment.TopCenter
         ) {
-            changeLocate(nowClimate)
-        }
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-        ){
-            lazyRowForShowWeather()
+            navigationBar(navController =  navController)
         }
     }
 }
--- app/src/main/java/com/example/ukweather/layout/weekScreen/weekScreen.kt
+++ app/src/main/java/com/example/ukweather/layout/weekScreen/weekScreen.kt
@@ -1,32 +1,81 @@
 package com.example.ukweather.layout.mainScreen
 
 import android.content.Context
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
+import androidx.compose.material.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.navigation.NavController
 import com.example.ukweather.db.DbManager
 import com.example.ukweather.getWeather.climate
+import com.example.ukweather.layout.common.navigationBar
+import com.example.ukweather.layout.todayScreen.lazyRowForShowWeather
+import com.example.ukweather.ui.theme.backgroundDarkBlue
+import com.example.ukweather.ui.theme.backgroundLightBlue
+
 @ExperimentalMaterialApi
 @Composable
 fun weekScreen(context: Context, climateState: MutableState<climate>, navController: NavController){
-    Column(
+    val nowClimate = climateState
+    val gradientGrayWhite = Brush.verticalGradient(0f to backgroundDarkBlue, 1000f to backgroundLightBlue)
+    val scaffoldState = rememberBottomSheetScaffoldState()
+    Box(
         modifier = Modifier
             .fillMaxSize()
+            .background(gradientGrayWhite)
     ) {
-        Text(text = "hello from week screen")
+        BottomSheetScaffold(
+            modifier = Modifier
+                .padding(bottom = 70.dp)
+                .zIndex(0.5f),
+            sheetContent = {
+                Box(
+                    modifier = Modifier
+                        .zIndex(1f),
+                    contentAlignment = Alignment.BottomCenter
+                ) {
+                    bottom(context, nowClimate)
+                }
+            },
+            sheetShape = RoundedCornerShape(topEnd = 30.dp, topStart = 30.dp),
+            scaffoldState = scaffoldState,
+            sheetPeekHeight = 50.dp,
+        ){
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(gradientGrayWhite)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .zIndex(2f),
+                    contentAlignment = Alignment.TopCenter
+                ) {
+                    changeLocate(nowClimate)
+                }
+            }
+        }
         Box(
             modifier = Modifier
-                .fillMaxSize(),
+                .fillMaxSize()
+                .zIndex(2f),
             contentAlignment = Alignment.BottomCenter
-        ){
-            bottom(context = context, climateState = climateState)
+        ) {
+            navigationBar(navController =  navController)
         }
     }
 }
