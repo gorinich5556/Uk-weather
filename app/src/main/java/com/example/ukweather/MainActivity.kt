114f1056b28614732c73d9843d9daf5584e2fda0
Egor Kovalev <egor.tharf@gmail.com>
2022-12-13 21:17:55 +0200

    add request for permission and description why we need this permission

--- app/src/main/java/com/example/ukweather/MainActivity.kt
+++ app/src/main/java/com/example/ukweather/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.ukweather
+import android.Manifest
+import android.app.AlertDialog
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.*
+import androidx.core.content.ContextCompat
 import com.example.ukweather.constanse.ConstanseDb
 import com.example.ukweather.constanse.ConstanseWeather
 import com.example.ukweather.db.DbManager
@@ -14,42 +22,16 @@ import com.example.ukweather.getWeather.getLocation
 import java.util.*
 
 class MainActivity : ComponentActivity() {
+    private lateinit var  pLauncher: ActivityResultLauncher<String>
     lateinit var myDbManager : DbManager
-    lateinit var temp: MutableState<Int>
     lateinit var climateState: MutableState<climate>
+    @RequiresApi(Build.VERSION_CODES.M)
     @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        myDbManager = DbManager(this)
-        myDbManager.openDb()
-
-        val date = Date() // given date
-        val calendar: Calendar = GregorianCalendar.getInstance()
-        val hoursNow = calendar.get(Calendar.HOUR_OF_DAY)
-        val dayNow = calendar.get(Calendar.DAY_OF_WEEK)
-        Log.d("ml", "current time is: ${hoursNow}")
-
-
-        //val currentLastTime = myDbManager.readDbData(ConstanseDb.TIME_COLUMN_VALUE_CURRENT_FOR)
-        //.d("ml", "hghfhg: ${currentLastTime.time}")
-
-
-        var a = 0
-        //Log.d("ml", "current time is: $curTime")
-        //Log.d("ml", "current time is: ${curTime.time}")
-        //Log.d("ml", "current time is: ${curTime.get}")
-        var clm = climate()
-        setContent {
-            climateState = remember { mutableStateOf(clm) }
-            var temp = remember { mutableStateOf(0) }
-            val getLoc = getLocation(this, climateState, myDbManager)
-            getLoc.init()
-            getLoc.getLoc(ConstanseWeather.CURRENT_WEATHER)
-            getLoc.getLoc(ConstanseWeather.FORECAST_OF_DAY)
-
-            Navigation(this, temp, climateState, myDbManager)
-        }
+        registerPermissionListener()
+        chekPermission()
 
     }
 
@@ -57,7 +39,81 @@ class MainActivity : ComponentActivity() {
         super.onDestroy()
         myDbManager.closeDb()
     }
+    @RequiresApi(Build.VERSION_CODES.M)
+    @ExperimentalMaterialApi
+    private fun chekPermission(){
+        when{
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED ->{
+                myDbManager = DbManager(this)
+                myDbManager.openDb()
+
+                val calendar: Calendar = GregorianCalendar.getInstance()
+                val hoursNow = calendar.get(Calendar.HOUR_OF_DAY)
+                Log.d("ml", "current time is: ${hoursNow}")
+
+                var clm = climate()
+                setContent {
+                    climateState = remember { mutableStateOf(clm) }
+                    var temp = remember { mutableStateOf(0) }
+                    val getLoc = getLocation(this, climateState, myDbManager)
+                    getLoc.init()
+                    getLoc.getLoc(ConstanseWeather.CURRENT_WEATHER)
+                    getLoc.getLoc(ConstanseWeather.FORECAST_OF_DAY)
+
+                    Navigation(this, temp, climateState, myDbManager)
+                }
+                    }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) ->{
+                buildPermissionAlertDialog()
+            }
+            else -> {
+                pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+    }
+
+    @ExperimentalMaterialApi
+    private fun buildPermissionAlertDialog(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Ви повинні дозволити використовувати GPS")
+        builder.setMessage("Щоб користуватися цим додатком вам потрібно дозволити GPS. Ми його використовуємо лише щоб" +
+                " дізнатись про погоду в вашій місцевості та показати її вам.")
+        builder.setPositiveButton("Дозволити"){dialog, i ->
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+        builder.show()
+    }
+
+    @ExperimentalMaterialApi
+    private fun registerPermissionListener(){
+        pLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            if(it){
+                myDbManager = DbManager(this)
+                myDbManager.openDb()
+
+                val calendar: Calendar = GregorianCalendar.getInstance()
+                val hoursNow = calendar.get(Calendar.HOUR_OF_DAY)
+                Log.d("ml", "current time is: ${hoursNow}")
+
+                var clm = climate()
+                setContent {
+                    climateState = remember { mutableStateOf(clm) }
+                    var temp = remember { mutableStateOf(0) }
+                    val getLoc = getLocation(this, climateState, myDbManager)
+                    getLoc.init()
+                    getLoc.getLoc(ConstanseWeather.CURRENT_WEATHER)
+                    getLoc.getLoc(ConstanseWeather.FORECAST_OF_DAY)
+
+                    Navigation(this, temp, climateState, myDbManager)
+                }
+            }else{
+                buildPermissionAlertDialog()
+            }
+        }
+    }
 }
+
 
 
 
